6a9f1956e27fd17c204119335db96ce7611aa114
EvertonLDS815 <billseverton12345@gmail.com>
2023-07-06 11:35:56 -0300

    site de filmes com API

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import Logo from '../../assets/logo.png'
+
+function Header() {
+
+    return (
+        <div>
+            <img src={Logo} alt="logo-de-movies" style={{width: 250}} />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,11 @@
+
+function Movies() {
+  return (
+    <div>
+      <h1>Filmes</h1>
+      <p>Essa é a rota Filmes</p>
+    </div>
+  )
+}
+
+export default Movies
