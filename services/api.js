a4595f3872e00035e58767e0a86ffa19f384be47
EvertonLDS815 <billseverton12345@gmail.com>
2023-07-13 12:38:50 -0300

    feat: add api with axios

---
+++ services/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    params: {
+        api_key: '5d7f0372fce5ae53a9d5a167b2ba490e',
+        language: 'pt-BR',
+        page: 1
+    }
+})
+
+export default api
