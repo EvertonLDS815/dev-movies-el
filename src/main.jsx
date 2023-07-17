ae31acd7225967974af7e2ffd9974fb2cf7149e9
EvertonLDS815 <billseverton12345@gmail.com>
2023-07-17 19:55:50 -0300

    feat: updating the GlobalStyles on main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './routes/routes'
-import GlobalStyle from './styles/globalStyles'
+import GlobalStyle from './styles/GlobalStyles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
